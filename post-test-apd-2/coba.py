fd699236dee7d00652de82fa75854de35588e1ae
larrynathanx <nathanaellarry@gmail.com>
2025-10-17 14:05:52 +0800

    up

---
+++ post-test-apd-2/coba.py
@@ -0,0 +1,13 @@
+buah = {"apel", "jeruk", "mangga", "apel"}
+buah = (["apel", "jeruk", "mangga", "apel"])
+print(buah)
+
+set_minum = {"air", "jus", "susu", "teh"}
+
+set_minum.add("kopi")
+print(set_minum)
+
+set_minum.remove("air")
+print(set_minum)
+
+set_minum.union
